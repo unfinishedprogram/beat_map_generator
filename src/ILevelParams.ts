fa4f4a986cad73a2c78615904074b2a859195614
Noah Labrecque <noah.labrecque@gmail.com>
2021-06-19 00:41:06 -0400

    removed commented code

--- src/ILevelParams.ts
+++ src/ILevelParams.ts
@@ -26,37 +26,9 @@ export interface ILevelParams {
   }
 }
 
-// export function strParamsToLevelParams(params: IStrLevelParams):ILevelParams {
-//   return {
-//     hand: ["10", "01", "11"].indexOf(params.filters.hand) as 0 | 1 | 2,
-//     targets: (() => {
-//       let boolTargs = [];
-//       for (let target of params.filters.targets) {
-//         boolTargs.push(target == "1");
-//       }
-//       return boolTargs
-//     })(),
-//     wall: {
-//       top: (params.filters.wall[0] == "1"),
-//       left: (params.filters.wall[1] == "1"),
-//       right: (params.filters.wall[2] == "1")
-//     },
-//     duration: [60, 120, 180][parseInt(params.filters.duration) - 1] as 60 | 120 | 180,
-//     distribution: (params.filters.distribution == "1") ? "blocked" : "random",
-//     rate: params.filters.rate,
-//     rhythm: params.filters.rhythm,
-//     song: params.filters.song,
-//     visDistance: parseInt(params.filters.visDistance),
-//   }
-// }
-
-
-
 export function peramToFileName(data: IStrLevelParams): string {
   return `hand${data.hand}-target${data.targets}-wall${data.wall}-duration${data.duration}-visdistance${data.visDistance}-distribution${data.distribution}-rhythm${data.rhythm}-song${data.song}`
 }
-
-// hand10-target0001000000-wall000-duration1-rate2-visdistance2-distribution1-rhythm1-song1
 
 export function fileNameToParams(file_name: string): IStrLevelParams {
   let params = file_name.split("-");
