78139b65e71b6560a27ad0b1d70886016d67d5b1
Noah <noah.labrecue@gmail.com>
2021-07-20 13:05:08 -0400

    Working

--- dist/server/server.js
+++ dist/server/server.js
@@ -5,53 +5,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var path_1 = __importDefault(require("path"));
-var beatmap_1 = require("../beatmap");
+var allBeatmapsFilter_1 = require("../allBeatmapsFilter");
 var app = express_1.default();
-var regex = new RegExp("^hand[01]{2}-target[01]{10}-wall[01]{3}-duration[123]-rate[1234]-visdistance[123]-distribution[12]-rhythm[123]-song.*$");
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
+var cors = require('cors');
+app.use(cors());
 var port = process.env.PORT || 8002;
 var router = express_1.default.Router();
-router.get('/', function (req, res) {
-    if ((typeof req.query["song_string"] != "string")) {
-        throw new Error('Invalid querry');
-    }
-    else if (!regex.test(req.query["song_string"])) {
-        throw new Error('Invalid level string');
-    }
-    else {
-        res.json(new beatmap_1.BeatMap(req.query["song_string"]).getBeatmapJson());
-    }
+router.post('/', function (req, res) {
+    res.json(allBeatmapsFilter_1.getAllBeatmaps(req.body));
 });
 router.get('/song', function (req, res) {
-    if ((typeof req.query["song_string"] != "string")) {
-        throw new Error('Invalid querry');
-    }
-    else if (!regex.test(req.query["song_string"])) {
-        throw new Error('Invalid level string');
-    }
-    else {
-        var song_string = req.query["song_string"];
-        if (!song_string)
-            return;
-        var folder_name = song_string.split("-")[4] + song_string.split("-")[3] + song_string.split("-")[8];
-        res.sendFile(path_1.default.join(__dirname, '../../src/server/songs', folder_name, 'song.egg'));
-    }
+    console.log(req.query["song"]);
+    var song_string = req.query["song"];
+    if (typeof song_string != "string")
+        return;
+    if (!song_string)
+        return;
+    res.sendFile(path_1.default.join(__dirname, '../../src/server/songs', song_string, 'song.egg'));
 });
 router.get('/cover', function (req, res) {
-    if ((typeof req.query["song_string"] != "string")) {
-        throw new Error('Invalid querry');
-    }
-    else if (!regex.test(req.query["song_string"])) {
-        throw new Error('Invalid level string');
-    }
-    else {
-        var song_string = req.query["song_string"];
-        if (!song_string)
-            return;
-        var folder_name = song_string.split("-")[4] + song_string.split("-")[3] + song_string.split("-")[8];
-        res.sendFile(path_1.default.join(__dirname, '../../src/server/songs', folder_name, 'cover.jpg'));
-    }
+    console.log(req.query["song"]);
+    var song_string = req.query["song"];
+    if (typeof song_string != "string")
+        return;
+    if (!song_string)
+        return;
+    res.sendFile(path_1.default.join(__dirname, '../../src/server/songs', song_string, 'cover.jpg'));
 });
 app.listen(port);
 app.use('/getsong', router);
